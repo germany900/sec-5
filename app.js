0fc3e5060e969b875fa49c54f34a15a2aa103aa6
German Manuel Juarez Morales <ninganegro@hotmail.com>
2020-02-21 12:11:05 -0600

    se creo el metodo get para por-hacer

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // const argv = require('yargs').argv;
 const argv = require('./config/yargs').argv;
 const colors = require('colors');
+const porHacer = require('./por-hacer/por-hacer');
 
 let comando = argv._[0];
 
@@ -8,11 +9,19 @@ let comando = argv._[0];
 
 switch (comando) {
     case 'crear':
-        console.log('Crear por hacer');
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
         break;
 
     case 'listar':
-        console.log('Mostrar todas las tareas por hacer');
+        let listado = porHacer.getListado();
+
+        for (let tarea of listado) {
+            console.log('========Por Hacer======='.green);
+            console.log(tarea.descripcion);
+            console.log('Estado: ', tarea.completado);
+            console.log('========================='.green);
+        }
         break;
 
     case 'actualizar':
